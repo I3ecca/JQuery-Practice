8febe9529e9d70d277233c6d413d0b3bdd53a357
RLant <rebeccalant@gmail.com>
2021-03-24 12:31:38 -0400

    Practice with JQuery

---
+++ base/js/script.js
@@ -0,0 +1,17 @@
+$(function() {
+  // jQuery goes here...
+
+  // Uncomment this line to fade out the red box on page load
+  // $(".red-box").fadeOut(2000);
+  // $(".red-box").fadeIn(3000);
+  //Very cool, fade to lets you set a duration and then set the opacity. 
+  // $(".red-box").fadeTo(2000, 0.2);
+  // $(".green-box").fadeTo(4000, 0.5);
+  // $(".blue-box").fadeTo(6000, 0.8);
+  $(".green-box").fadeTo(1000, 0);
+  // $(".green-box").fadeToggle();
+
+//2000miliseconds is 2seconds
+});
+
+
