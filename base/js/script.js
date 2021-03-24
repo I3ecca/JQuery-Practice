607325fab4178628c13c26fcc0321062a14afbe9
RLant <rebeccalant@gmail.com>
2021-03-24 13:14:38 -0400

    animations

--- base/js/script.js
+++ base/js/script.js
@@ -8,10 +8,23 @@ $(function() {
   // $(".red-box").fadeTo(2000, 0.2);
   // $(".green-box").fadeTo(4000, 0.5);
   // $(".blue-box").fadeTo(6000, 0.8);
-  $(".green-box").fadeTo(1000, 0);
+  //fadeTo is useful when you want something to disapper and not change the layout. Display sets it to whatever it was before where the fadetoggle will remove the display property. 
+  // $(".green-box").fadeTo(1000, 0);
   // $(".green-box").fadeToggle();
 
 //2000miliseconds is 2seconds
+
+$(".blue-box").show();
+
+// console.log($(".blue-box"));
+
+// $(".blue-box").slideUp(800);
+// $(".blue-box").slideDown(600);
+
+$("p").hide();
+$("p").slideDown(1000);
+
+
 });
 
 
